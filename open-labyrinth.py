cdcd6a8438c56049daf4425ff2bdb9968bc1b366
Mikhail Pyrev <mikhail.pyrev@gmail.com>
2016-12-14 22:54:35 +0500

    Solved labyrinth puzzle

--- open-labyrinth.py
+++ open-labyrinth.py
@@ -7,38 +7,63 @@ FINISH = (10, 10)
 
 def get_manhattan_score(coords):
     return (FINISH[0]-coords[0]) + (FINISH[1]-coords[1])
-    
-    
+
+
 def make_move(coords, move):
-    return [coords[0]+move[0], coords[1]+move[1]]
+    return coords[0]+move[0], coords[1]+move[1]
 
 
 class SearchNode:
     coords = None
     move_number = None
     previous = None
-    
-    def __init__(self, coords, move_number, previous):
+    visited = None
+
+    def __init__(self, coords, move_number, previous, visited):
         self.coords = coords
         self.move_number = move_number
         self.previous = previous
-        
+        self.visited = visited
+
     def get_priority(self):
         return self.move_number + get_manhattan_score(self.coords)
-        
+
     def is_goal(self):
         return self.coords == FINISH
-        
+
     def __lt__(self, other):
         return self.get_priority() < other.get_priority()
-        
+
     def __repr__(self):
-        return 'SearchNode({})'.format(self.coords)
+        v = [str(self.coords), str(self.move_number)]
+        if self.previous is not None:
+            v.append('SearchNode({}, {}, ...)'.format(self.previous.coords, self.previous.move_number))
+        return 'SearchNode({})->{}'.format(', '.join(v), self.get_priority())
+
+    def get_steps(self):
+        l = []
+        node = self
+        while node is not None:
+            l.insert(0, node.coords)
+            node = node.previous
+        return l
+
+    def get_moves(self):
+        moves = []
+        steps = self.get_steps()
+        for x, y in zip(steps[:-1], steps[1:]):
+            moves.append((y[0]-x[0], y[1]-x[1]))
+        return moves
+
+
+def moves_to_str(moves):
+    mapping = {(1,0): 'S', (0,1): 'E', (-1,0): 'N', (0,-1): 'W'}
+    return ''.join([mapping[m] for m in moves])
 
 
 def checkio(maze_map):
     queue = []
-    heappush(queue, SearchNode([1,1], 0, None))
+    heappush(queue, SearchNode(START, 0, None, {START}))
     result_node = None
     while queue:
         node = heappop(queue)
@@ -49,19 +74,18 @@ def checkio(maze_map):
             coords = make_move(node.coords, move)
             # Make sure we are not coming back
             if maze_map[coords[0]][coords[1]] == 0:
-                if node.previous and node.previous.coords == coords:
+                if coords in node.visited:
                     continue
-                heappush(queue, SearchNode(coords, node.move_number+1, node))
-    print(result_node)
-    return result
-    #return "SSSSSEENNNEEEEEEESSWWWWSSSEEEESS"
+                node.visited.add(coords)
+                heappush(queue, SearchNode(coords, node.move_number+1, node, node.visited))
+    return moves_to_str(result_node.get_moves())
 
 
 if __name__ == '__main__':
-    #This code using only for self-checking and not necessary for auto-testing
+    # This code using only for self-checking and not necessary for auto-testing
     def check_route(func, labyrinth):
         MOVE = {"S": (1, 0), "N": (-1, 0), "W": (0, -1), "E": (0, 1)}
-        #copy maze
+        # copy maze
         route = func([row[:] for row in labyrinth])
         pos = (1, 1)
         goal = (10, 10)
